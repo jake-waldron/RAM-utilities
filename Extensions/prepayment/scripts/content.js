d7b8a8f59258afbb77e5c4a4fe7c6608d89db12f
Jake Waldron <jakewaldron@gmail.com>
2023-02-27 11:13:18 -0500

    add don't/use credit checkbox and logic

--- Extensions/prepayment/scripts/content.js
+++ Extensions/prepayment/scripts/content.js
@@ -33,6 +33,29 @@ function createCreditDisplay(availableCredit) {
 	return creditDisplay;
 }
 
+function addDontUseCreditButton() {
+	// Add checkbox toggle for not using prepayment
+	const useCreditCheckbox = document.createElement('input');
+	useCreditCheckbox.type = 'checkbox';
+	useCreditCheckbox.id = 'prepayment-checkbox';
+	useCreditCheckbox.checked = false;
+
+	const useCreditLabel = document.createElement('label');
+	useCreditLabel.textContent = "Don't Use Available Credit";
+	useCreditLabel.htmlFor = 'prepayment-checkbox';
+	useCreditLabel.classList.add('m-l-sm');
+
+	const usePaymentContainer = document.createElement('div');
+	usePaymentContainer.classList.add('form-group', 'pull-right');
+	usePaymentContainer.appendChild(useCreditCheckbox);
+	usePaymentContainer.appendChild(useCreditLabel);
+
+	const paymentContainer = document.querySelector('.cashPanel').parentElement;
+	paymentContainer.appendChild(usePaymentContainer);
+
+	return useCreditCheckbox;
+}
+
 //TODO : Add "Don't use prepayment" button
 function handlePrepayment() {
 	const { orderTotal, availableCredit, paymentForm, paymentInput } = getInitialElements();
@@ -45,12 +68,33 @@ function handlePrepayment() {
 		const creditDisplay = createCreditDisplay(availableCredit);
 		balanceDisplay.appendChild(creditDisplay);
 
+		const checkbox = addDontUseCreditButton();
+		let amountToApply = paymentNeeded;
+		let maxPaymentAmount = paymentNeeded;
+
+		// Set default values to use credit
 		const payInFullButton = document.querySelector('#PayFullAmount');
 		const payInFullButtonText = payInFullButton.querySelector('span');
 		payInFullButtonText.textContent = 'Pay Remaining Balance';
+		paymentInput.max = `${maxPaymentAmount}`;
+
+		checkbox.addEventListener('change', () => {
+			if (!checkbox.checked) {
+				// Use credit
+				maxPaymentAmount = paymentNeeded;
+				amountToApply = paymentNeeded;
+				payInFullButtonText.textContent = 'Pay Remaining Balance';
+			} else {
+				// Pay full amount
+				maxPaymentAmount = orderTotal;
+				amountToApply = orderTotal;
+				payInFullButtonText.textContent = 'Pay in Full';
+			}
+		});
+
 		payInFullButton.addEventListener('click', () => {
-			paymentInput.max = `${paymentNeeded}`;
-			paymentInput.value = `${paymentNeeded}`;
+			paymentInput.max = `${maxPaymentAmount}`;
+			paymentInput.value = `${amountToApply}`;
 		});
 	}
 }
