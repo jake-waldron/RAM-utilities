cfbceab29e9eddcd28d7abd76376bf3a44b7a46d
Jake Waldron <jakewaldron@gmail.com>
2023-02-27 07:02:52 -0500

    split into functions for clarity

--- Bookmarks/edit-email-send-invoice.js
+++ Bookmarks/edit-email-send-invoice.js
@@ -1,12 +1,14 @@
 javascript: (function () {
-	if (document.URL.startsWith('https://amp.reynoldsam.com/Order/SendInvoice')) {
+	function removeEmailStuff() {
 		const iframeContent = document.querySelector('iframe').contentWindow.document;
 		const emailLines = Array.from(iframeContent.querySelectorAll('p'));
 		const dearLine = emailLines[0];
 		const contactLine = emailLines[2];
 		dearLine.remove();
 		contactLine.textContent = contactLine.textContent.replace('Jake Waldron', 'us');
+	}
 
+	function sendInvoice() {
 		const orderBalance = document.querySelector('#orderBalance')?.value;
 		const paymentAmount = document.querySelector('#Request_UnappliedPayments_0__ApplyToOrderAmount')?.max;
 		const secondPayment = document.querySelector('#Request_UnappliedPayments_1__ApplyToOrderAmount');
@@ -17,5 +19,11 @@ javascript: (function () {
 			document.querySelector('#ApplyPrepayment')?.click();
 			document.querySelector('#SubmitButton').click();
 		}
+	}
+
+	if (document.URL.startsWith('https://amp.reynoldsam.com/Order/SendInvoice')) {
+		removeEmailStuff();
+
+		sendInvoice();
 	}
 })();
