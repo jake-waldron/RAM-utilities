9fa4547fc6d081ea41c17e9957eeba25bdb88ed1
Jake Waldron <jakewaldron@gmail.com>
2023-03-25 15:09:19 -0400

    fixed sort by size

--- Extensions/search/src/utils/sort.ts
+++ Extensions/search/src/utils/sort.ts
@@ -1,3 +1,5 @@
+import { group } from "console"
+
 type Product = {
   name: string
   score: number
@@ -6,40 +8,44 @@ type Product = {
 
 function sort(arrayToSort) {
   const scores = arrayToSort.map((product) => product.score)
+
   const scoreSet = new Set(scores)
 
   const groupedByScore = [...scoreSet].map((score) => {
     return arrayToSort.filter((product) => product.score === score)
   })
 
-  const sortedArray = groupedByScore.flatMap((group) => sortArray(group))
-  console.log(sortedArray)
+  const sortedArray = groupedByScore.map((group) => sortArray(group))
   return sortedArray
+}
 
-  function sortArray(arr): Product[] {
-    const order = {
-      "PINT UNIT": 0,
-      "TRIAL SIZE": 0,
-      "1-GAL UNIT": 1,
-      "5-GAL UNIT": 2,
-      UNKNOWN: 3
-    }
-
-    let newOrder = {}
-    arr.forEach((entry: Product) => {
-      const sizeRegex = /\b(PINT UNIT|TRIAL SIZE|1-GAL UNIT|5-GAL UNIT)\b/
-      const size = entry.name.match(sizeRegex)
-      const sizeName = size ? size[1] : "UNKNOWN"
-      let orderIndex = order[sizeName]
-      if (newOrder[orderIndex]) {
-        orderIndex = parseInt(Object.keys(newOrder).at(-1)) + 1
-      }
-      newOrder[orderIndex] = entry
-    })
-    return Object.values(newOrder)
+function sortArray(arr): Product[] {
+  const order = {
+    "PINT UNIT": 0,
+    "TRIAL SIZE": 0,
+    "1-GAL UNIT": 1,
+    "5-GAL UNIT": 2,
+    "5-GAL PART": 99,
+    UNKNOWN: 3
   }
+
+  let newOrder = {}
+  arr.forEach((entry: Product) => {
+    const sizeRegex =
+      /\b(PINT UNIT|TRIAL SIZE|1-GAL UNIT|5-GAL UNIT|5-GAL PART)\b/
+    const size = entry.name.match(sizeRegex)
+    const sizeName = size ? size[1] : "UNKNOWN"
+    let orderIndex = order[sizeName]
+    if (newOrder[orderIndex]) {
+      orderIndex = parseInt(Object.keys(newOrder).at(-1)) + 1
+    }
+    newOrder[orderIndex] = entry
+  })
+  return Object.keys(newOrder)
+    .sort((a, b) => +a - +b)
+    .map((key) => newOrder[key])
 }
 
 export function sortBySize(arrayToSort): Product[] {
-  return sort(arrayToSort)
+  return sort(arrayToSort).flat()
 }
