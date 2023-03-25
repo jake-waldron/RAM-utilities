35379a6cbd883c328b055889492c9d3da25c604c
Jake Waldron <jakewaldron@gmail.com>
2023-03-25 16:30:07 -0400

    include 5-gal parts

--- Extensions/search/src/utils/sort.ts
+++ Extensions/search/src/utils/sort.ts
@@ -23,16 +23,15 @@ function sortArray(arr): Product[] {
   const order = {
     "PINT UNIT": 0,
     "TRIAL SIZE": 0,
-    "1-GAL UNIT": 1,
-    "5-GAL UNIT": 2,
+    "1-GAL": 1,
+    "5-GAL": 2,
     "5-GAL PART": 99,
     UNKNOWN: 3
   }
 
   let newOrder = {}
   arr.forEach((entry: Product) => {
-    const sizeRegex =
-      /\b(PINT UNIT|TRIAL SIZE|1-GAL UNIT|5-GAL UNIT|5-GAL PART)\b/
+    const sizeRegex = /\b(PINT UNIT|TRIAL SIZE|1-GAL|5-GAL|5-GAL PART)\b/
     const size = entry.name.match(sizeRegex)
     const sizeName = size ? size[1] : "UNKNOWN"
     let orderIndex = order[sizeName]
