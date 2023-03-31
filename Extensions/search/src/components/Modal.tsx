e5b4bf681e1acd9201450328e5400f9813185974
Jake Waldron <jakewaldron@gmail.com>
2023-03-31 08:49:50 -0400

    split components

--- Extensions/search/src/components/Modal.tsx
+++ Extensions/search/src/components/Modal.tsx
@@ -1,97 +1,97 @@
 import useStore from "../store"
 import React, { ReactNode } from "react"
+import { useQuery } from "react-query"
+
+async function findProducts(search: string) {
+  const data = await fetch(`${process.env.API}/search?q=${search}`)
+
+  return await data.json()
+}
 
 export default function Modal() {
-  const state = useStore()
+  const { searchBarRef, showModal, toggleModal } = useStore()
 
+  const [inputValue, setInputValue] = React.useState("")
   const [searchTerm, setSearchTerm] = React.useState("")
-  const [results, setResults] = React.useState([])
 
+  // Fetch products from API when search term changes
+  const queryData = useQuery(["products", searchTerm], () =>
+    findProducts(searchTerm)
+  )
+
+  // Set initial value of input if AMP search bar has a value on mount
   React.useEffect(() => {
-    if (state.searchBarRef) {
-      setSearchTerm(state.searchBarRef.current?.value || "")
+    if (searchBarRef) {
+      setInputValue(searchBarRef.current?.value || "")
     }
-  }, [state.showModal])
+  }, [showModal])
 
+  // Debounce input value and set search term after timeout
   React.useEffect(() => {
-    async function getResults() {
-      const data = await fetch(`${process.env.API}/search?q=${searchTerm}`)
-
-      const apiResponse = await data.json()
-
-      const { products, error } = apiResponse.data
-      console.log(products)
-      setResults(products)
-    }
     const debounce = setTimeout(() => {
-      if (!searchTerm) return setResults([])
-      if (searchTerm) {
-        getResults()
-      }
+      setSearchTerm(inputValue)
     }, 500)
 
     return () => clearTimeout(debounce)
-  }, [searchTerm])
-
-  function closeModal(e: React.MouseEvent<HTMLDivElement>) {
-    // e.preventDefault()
-    if (e.target === e.currentTarget) {
-      state.toggleModal()
-    }
-  }
-
-  function handleSelect(partNum: string) {
-    if (partNum === "") return
-    state.searchBarRef.current.value = partNum
-    state.searchBarRef.current.focus()
-    state.toggleModal()
-  }
+  }, [inputValue])
 
   return (
     <>
-      <ModalBackdrop closeModal={closeModal}>
+      <ModalBackdrop>
         <div className="z-10 flex flex-col rounded-xl bg-white p-4 text-3xl text-slate-400  ">
           {/* ADD MODAL COMPONENT HERE */}
           <input
             type="text"
-            value={searchTerm || ""}
+            value={inputValue || ""}
             placeholder="Search for product..."
-            onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={(e) => setInputValue(e.target.value)}
           />
           <hr />
-          {results && (
-            <ul>
-              {results.map((result) => (
-                <li key={result.partNum}>
-                  <p onClick={() => handleSelect(result.partNum)}>
-                    {result.name}
-                  </p>
-                </li>
-              ))}
-            </ul>
-          )}
-          {results.length === 0 && (
-            <p className="text-center">No results found</p>
-          )}
+          {searchTerm !== "" && <Results queryData={queryData} />}
         </div>
       </ModalBackdrop>
     </>
   )
 }
 
-function ModalBackdrop({
-  children,
-  closeModal
-}: {
-  children: ReactNode
-  closeModal: (e: React.MouseEvent) => void
-}) {
+function ModalBackdrop({ children }: { children: ReactNode }) {
+  const { toggleModal } = useStore()
   return (
     <div
       id="modal-backdrop"
       className="fixed z-0 flex h-screen w-screen items-center justify-center bg-gray-800/25"
-      onClick={closeModal}>
+      onClick={() => toggleModal()}>
       {children}
     </div>
+  )
+}
+
+function Results({ queryData }) {
+  const { searchBarRef, toggleModal } = useStore()
+  // console.log("results are: ", results)
+  const { isLoading, isError, error, data } = queryData
+
+  function handleSelect(partNum: string) {
+    if (partNum === "") return
+    searchBarRef.current.value = partNum
+    searchBarRef.current.focus()
+    toggleModal()
+  }
+
+  if (isLoading) return <p className="text-center">Loading...</p>
+  if (isError) return <p className="text-center">Error: {error.message}</p>
+
+  const { products } = data.data
+
+  return products.length > 0 ? (
+    <ul>
+      {products.map((result) => (
+        <li key={result.partNum}>
+          <p onClick={() => handleSelect(result.partNum)}>{result.name}</p>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p className="text-center">No results found</p>
   )
 }
