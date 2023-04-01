4184b556dbb1ef5b39c0c372e434d23df97cc341
Jake Waldron <jakewaldron@gmail.com>
2023-04-01 12:36:15 -0400

    add feedback

--- Extensions/search/src/components/Modal/Results.tsx
+++ Extensions/search/src/components/Modal/Results.tsx
@@ -1,8 +1,11 @@
 import useStore from "@store"
 import Feedback from "./Feedback"
+import { CgSpinner } from "react-icons/cg"
+import React from "react"
 
 export default function Results({ queryData }) {
   const { searchBarRef, toggleModal } = useStore()
+  const [showProducts, setShowProducts] = React.useState(true)
   // console.log("results are: ", results)
   const { isLoading, isError, error, data } = queryData
 
@@ -13,15 +16,27 @@ export default function Results({ queryData }) {
     toggleModal()
   }
 
-  if (isLoading) return <p className="text-center">Searching...</p>
+  function hideProducts() {
+    setShowProducts(false)
+  }
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center">
+        <CgSpinner className="mx-4 animate-spin text-3xl" />
+        <p className="text-center">Searching...</p>
+      </div>
+    )
   if (isError) return <p className="text-center">Error: {error.message}</p>
 
   const { products } = data
 
   return (
     <>
-      <ResultsList products={products} handleSelect={handleSelect} />
-      <Feedback apiResponse={data} />
+      {showProducts && (
+        <ResultsList products={products} handleSelect={handleSelect} />
+      )}
+      <Feedback apiResponse={data} hideProducts={hideProducts} />
     </>
   )
 }
