e5402062436776b3b0fdfb8ce969a64b9cdfe6a5
Jake Waldron <jakewaldron@gmail.com>
2023-04-02 14:49:11 -0400

    add report issue feedback

--- Extensions/search/src/components/Modal/Feedback.tsx
+++ Extensions/search/src/components/Modal/Feedback.tsx
@@ -1,58 +1,181 @@
+import { queryClient } from "@contents/content"
 import React from "react"
+import { CgKey, CgSpinner } from "react-icons/cg"
+import { useQuery } from "react-query"
 
 // THIS IS WEIRD AND NEEDS FIXING
 
 export default function Feedback({ apiResponse, hideProducts }) {
   const { products } = apiResponse
 
+  React.useEffect(() => {
+    queryClient.resetQueries({ queryKey: "email" })
+  }, [apiResponse])
+
   return (
-    <>
+    <div className="mt-2 flex justify-center">
       {products.length > 0 ? (
         <ReportIssue apiResponse={apiResponse} hideProducts={hideProducts} />
       ) : (
         <RequestProduct apiResponse={apiResponse} />
       )}
-    </>
+    </div>
   )
 }
 
 function FeedbackButton({ children, onClick }) {
   return (
     <button
-      className="mt-2 w-max self-center text-center text-[13px] text-red-500"
+      className=" w-max self-center text-center text-[13px] text-red-500"
       onClick={onClick}>
       {children}
     </button>
   )
 }
 
+async function sendEmail(emailData) {
+  const response = await fetch(`${process.env.API}/search/email`, {
+    method: "POST",
+    body: JSON.stringify(emailData),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  if (!response.ok) throw new Error("Error sending email")
+  return response.json()
+}
+
 function RequestProduct({ apiResponse }) {
-  function handleClick() {
-    console.log("request product")
-    console.log(apiResponse)
+  const emailData = {
+    searchTerm: apiResponse.searchTerm,
+    apiResponse,
+    feedbackType: "Product Request",
+    issue: "Product Request"
   }
+
+  const { data, isLoading, isError, refetch } = useQuery(
+    "email",
+    () => sendEmail(emailData),
+    { enabled: false }
+  )
+
+  async function handleClick() {
+    refetch()
+  }
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center">
+        <CgSpinner className="mx-4 animate-spin text-3xl" />
+        <p className="text-center">Requesting product...</p>
+      </div>
+    )
+
+  if (isError)
+    return <p className="text-center">Sorry, something went wrong!</p>
+
+  if (data) return <p className="text-center">Product requested!</p>
 
   return <FeedbackButton onClick={handleClick}>Request Product</FeedbackButton>
 }
 
-function sendEmail(emailOptions) {
-  // const { subject, body } = emailOptions
-  const { searchTerm, apiResponse, feedbackType } = emailOptions
-  const email = `mailto:jakewaldron+ram@gmail.com?subject=Search - "${searchTerm}"&body=Issue: ${feedbackType}%0d%0a%0d%0aSearched for: "${searchTerm}"%0d%0a%0d%0aAPI Response:%0d%0a${JSON.stringify(
-    apiResponse
-  )}`
-  window.open(email)
-  console.log(email)
-}
+function FeedbackForm({ apiResponse }) {
+  const otherRadioRef = React.useRef<HTMLInputElement>(null)
+  const otherDescRef = React.useRef<HTMLInputElement>(null)
+  let emailData = {}
 
-function FeedbackForm() {
+  const { data, isLoading, isError, refetch } = useQuery(
+    "email",
+    () => sendEmail(emailData),
+    { enabled: false }
+  )
+
   function handleSubmit(e) {
     e.preventDefault()
-    console.log("submit form")
+    const form = e.target
+    const formData = new FormData(form)
+    const formJson = Object.fromEntries(formData.entries())
+    const { issue, otherDescription } = formJson
+    console.log(formJson)
+
+    emailData = {
+      searchTerm: apiResponse.searchTerm,
+      apiResponse,
+      feedbackType: "Product Request",
+      issue: otherDescription ? otherDescription : issue
+    }
+
+    refetch()
   }
+
+  if (isLoading) return <p className="text-center">Sending feedback...</p>
+
+  if (isError)
+    return <p className="text-center">Sorry, something went wrong!</p>
+
+  if (data) return <p className="text-center">Feedback sent!</p>
+
   return (
     <form onSubmit={handleSubmit} className="flex flex-col items-center">
-      <p>FEEDBACK FORM</p>
+      {/* // four radio inputs with labels and values of "Too many results" "Not
+      enough results" "Wrong results" "Other" // text input with label "Please
+      describe your issue" // submit button */}
+      <div className="flex flex-col">
+        <label htmlFor="tooManyResults">
+          <input
+            type="radio"
+            name="issue"
+            id="tooManyResults"
+            value="Too many results"
+            className="mr-3"
+          />
+          Too many results
+        </label>
+        <label htmlFor="notEnoughResults">
+          <input
+            type="radio"
+            name="issue"
+            id="notEnoughResults"
+            value="Not enough results"
+            className="mr-3"
+          />
+          Not enough results
+        </label>
+        <label htmlFor="wrongResults">
+          <input
+            type="radio"
+            name="issue"
+            id="wrongResults"
+            value="Wrong results"
+            className="mr-3"
+          />
+          Wrong results
+        </label>
+        <div>
+          <label htmlFor="other">
+            <input
+              ref={otherRadioRef}
+              type="radio"
+              name="issue"
+              id="other"
+              value="Other"
+              className="mr-3"
+              onClick={() => otherDescRef.current?.focus()}
+            />
+            Other
+          </label>
+          <input
+            type="text"
+            ref={otherDescRef}
+            name="otherDescription"
+            id="otherDescription"
+            className="ml-2 border pl-2"
+            onFocus={() => {
+              otherRadioRef.current.checked = true
+            }}
+          />
+        </div>
+      </div>
       <button className="mt-2 w-max self-center text-center text-[13px] text-red-500">
         Submit
       </button>
@@ -78,7 +201,7 @@ function ReportIssue({ apiResponse, hideProducts }) {
       {!showForm && (
         <FeedbackButton onClick={handleClick}>Report Issue</FeedbackButton>
       )}
-      {showForm && <FeedbackForm />}
+      {showForm && <FeedbackForm apiResponse={apiResponse} />}
     </>
   )
 }
