e40f1100ce99f3f9ae60145c4de783c56999048b
Jake Waldron <jakewaldron@gmail.com>
2023-04-02 12:38:25 -0400

    simplified components

--- Extensions/search/src/components/Modal/Feedback.tsx
+++ Extensions/search/src/components/Modal/Feedback.tsx
@@ -3,85 +3,36 @@ import React from "react"
 // THIS IS WEIRD AND NEEDS FIXING
 
 export default function Feedback({ apiResponse, hideProducts }) {
-  const [showButton, setShowButton] = React.useState(true)
-  const [showForm, setShowForm] = React.useState(false)
-  const [feedbackSent, setFeedbackSent] = React.useState(false)
-
-  let { products, searchTerm } = apiResponse
-  React.useEffect(() => {
-    products = apiResponse.products
-    searchTerm = apiResponse.searchTerm
-  }, [apiResponse])
-
-  console.log(products)
-  function handleProductRequest() {
-    console.log("Request Product")
-    console.log(apiResponse)
-    sendEmail({
-      searchTerm,
-      apiResponse,
-      feedbackType: "Product Request"
-    })
-    setShowButton(false)
-    setFeedbackSent(true)
-  }
-
-  function handleReportIssue() {
-    console.log("Report Issue")
-    console.log(apiResponse)
-    sendEmail({
-      searchTerm,
-      apiResponse,
-      feedbackType: "Report Issue"
-    })
-    hideProducts()
-    setShowButton(false)
-    setShowForm(true)
-  }
-
-  const buttonProps = {
-    onClick: products.length === 0 ? handleProductRequest : handleReportIssue,
-    display: products.length === 0 ? "Request Product" : "Report Issue"
-  }
+  const { products } = apiResponse
 
   return (
     <>
-      {products.length === 0 && <RequestProduct />}
-      {products.length > 0 && <ReportIssue onClick={hideProducts} />}
-      {/* {showButton && (
-        <FeedbackButton onClick={buttonProps.onClick}>
-          {buttonProps.display}
-        </FeedbackButton>
+      {products.length > 0 ? (
+        <ReportIssue apiResponse={apiResponse} hideProducts={hideProducts} />
+      ) : (
+        <RequestProduct apiResponse={apiResponse} />
       )}
-      {showForm && <FeedbackForm />}
-      {feedbackSent && (
-        <p className="mt-2 text-center text-[13px]">Email sent!</p>
-      )} */}
     </>
   )
 }
 
-function FeedbackButton({ children, ShowAfterClick }) {
-  const [showButton, setShowButton] = React.useState(true)
-  const [showAfter, setShowAfter] = React.useState(false)
+function FeedbackButton({ children, onClick }) {
+  return (
+    <button
+      className="mt-2 w-max self-center text-center text-[13px] text-red-500"
+      onClick={onClick}>
+      {children}
+    </button>
+  )
+}
 
+function RequestProduct({ apiResponse }) {
   function handleClick() {
-    setShowButton(false)
-    setShowAfter(true)
+    console.log("request product")
+    console.log(apiResponse)
   }
 
-  return (
-    <>
-      {showButton && (
-        <button
-          className="mt-2 w-max self-center text-center text-[13px] text-red-500"
-          onClick={handleClick}>
-          {children}
-        </button>
-      )}
-      {showAfter && <ShowAfterClick />}
-    </>
-  )
+  return <FeedbackButton onClick={handleClick}>Request Product</FeedbackButton>
 }
 
 function sendEmail(emailOptions) {
@@ -109,28 +60,26 @@ function FeedbackForm() {
   )
 }
 
-function RequestProduct() {
-  function ShowAfterClick() {
-    return <p className="mt-2 text-center text-[13px]">Email sent!</p>
+function ReportIssue({ apiResponse, hideProducts }) {
+  const [showForm, setShowForm] = React.useState(false)
+
+  React.useEffect(() => {
+    setShowForm(false)
+  }, [apiResponse])
+
+  function handleClick() {
+    console.log("report issue")
+    console.log(apiResponse)
+    hideProducts()
+    setShowForm(true)
   }
-
   return (
-    <FeedbackButton ShowAfterClick={ShowAfterClick}>
-      Request Product
-    </FeedbackButton>
-  )
-}
-
-function ReportIssue({ onClick }) {
-  function ShowAfterClick() {
-    onClick()
-    return <FeedbackForm />
-  }
-
-  return (
-    <FeedbackButton ShowAfterClick={ShowAfterClick}>
-      Request Product
-    </FeedbackButton>
+    <>
+      {!showForm && (
+        <FeedbackButton onClick={handleClick}>Report Issue</FeedbackButton>
+      )}
+      {showForm && <FeedbackForm />}
+    </>
   )
 }
 
